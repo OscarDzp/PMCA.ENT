4ecd99722dcc3d47191d74f2d6a9360a4e2180ad
Karinaporras <107158954+Karinaporras@users.noreply.github.com>
2022-07-10 13:11:24 -0600

    entidades
    activos y empleados

---
+++ src/Clases/clsAutos.java
@@ -0,0 +1,154 @@
+
+package Clases;
+
+/**
+ *
+ * @author karii
+ */
+public class clsAutos {
+    private String marca;
+    private String modelo;
+    private int año;
+    private String estilo;
+    private String motor;
+    private String cilindrada;
+    private String combustible;
+    private int pasajeros;
+    private String chasis;
+    private String transmision;
+    private String kilometraje;
+    private String estado;
+    private int valor;
+    private int idAutos;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public String getEstilo() {
+        return estilo;
+    }
+
+    public void setEstilo(String estilo) {
+        this.estilo = estilo;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
+
+    public String getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(String cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    public String getCombustible() {
+        return combustible;
+    }
+
+    public void setCombustible(String combustible) {
+        this.combustible = combustible;
+    }
+
+    public int getPasajeros() {
+        return pasajeros;
+    }
+
+    public void setPasajeros(int pasajeros) {
+        this.pasajeros = pasajeros;
+    }
+
+    public String getChasis() {
+        return chasis;
+    }
+
+    public void setChasis(String chasis) {
+        this.chasis = chasis;
+    }
+
+    public String getTransmision() {
+        return transmision;
+    }
+
+    public void setTransmision(String transmision) {
+        this.transmision = transmision;
+    }
+
+    public String getKilometraje() {
+        return kilometraje;
+    }
+
+    public void setKilometraje(String kilometraje) {
+        this.kilometraje = kilometraje;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+        
+    }
+    
+      public clsAutos () {}
+      
+      public clsAutos (int idAutos) {
+         this.idAutos = idAutos;
+      }
+      
+      
+    public clsAutos(String marca, String modelo, int año, String estilo, String motor, String cilindrada, String combustible, int pasajeros, String chasis, String transmision, String kilometraje, String estado, int valor) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+        this.estilo = estilo;
+        this.motor = motor;
+        this.cilindrada = cilindrada;
+        this.combustible = combustible;
+        this.pasajeros = pasajeros;
+        this.chasis = chasis;
+        this.transmision = transmision;
+        this.kilometraje = kilometraje;
+        this.estado = estado;
+        this.valor = valor;
+    }
+    
+    
+    
+}
--- src/Clases/clsEmpleado.java
+++ src/Clases/clsEmpleado.java
@@ -3,8 +3,155 @@ package Clases;
 
 /**
  *
- * @author oscar zumbaddo porras
+ * @author karii
  */
 public class clsEmpleado {
+    
+    private int idEmpleado;
+    private String Codigo;
+    private String tipoIdentificacion;
+    private String Nombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private String Cargo;
+    private String Direccion;
+    private String Correo;
+    private String telefono;
+    private String modalidad;
+    private String Departamento;private boolean estado;
+    private String codigo;
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getCodigo() {
+        return Codigo;
+    }
+
+    public void setCodigo(String Codigo) {
+        this.Codigo = Codigo;
+    }
+
+    public String getTipoIdentificacion() {
+        return tipoIdentificacion;
+    }
+
+    public void setTipoIdentificacion(String tipoIdentificacion) {
+        this.tipoIdentificacion = tipoIdentificacion;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public String getCargo() {
+        return Cargo;
+    }
+
+    public void setCargo(String Cargo) {
+        this.Cargo = Cargo;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getModalidad() {
+        return modalidad;
+    }
+
+    public void setModalidad(String modalidad) {
+        this.modalidad = modalidad;
+    }
+
+    public String getDepartamento() {
+        return Departamento;
+    }
+
+    public void setDepartamento(String Departamento) {
+        this.Departamento = Departamento;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    public clsEmpleado() {
+    }
+
+    public clsEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public clsEmpleado(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public clsEmpleado(int idEmpleado, String Codigo, String tipoIdentificacion, String Nombre, String primerApellido, String segundoApellido, String Cargo, String Direccion, String Correo, String telefono, String modalidad, String Departamento, boolean estado, String codigo) {
+        this.idEmpleado = idEmpleado;
+        this.Codigo = Codigo;
+        this.tipoIdentificacion = tipoIdentificacion;
+        this.Nombre = Nombre;
+        this.primerApellido = primerApellido;
+        this.segundoApellido = segundoApellido;
+        this.Cargo = Cargo;
+        this.Direccion = Direccion;
+        this.Correo = Correo;
+        this.telefono = telefono;
+        this.modalidad = modalidad;
+        this.Departamento = Departamento;
+        this.estado = estado;
+        this.codigo = codigo;
+    }
+    
     
 }
